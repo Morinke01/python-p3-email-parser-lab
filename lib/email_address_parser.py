15416bb17a2f6c95715f4d1f9eb8697f36e7d197
Morinke01 <ejulius250@gmail.com>
2023-08-31 14:15:22 +0300

    Assignment Completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,21 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+    
+    def parse(self):
+        # Regular expression pattern to match email addresses
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        
+        # Use re.findall() to find all email addresses in the string
+        email_addresses = re.findall(email_pattern, self.email_string)
+        
+        return email_addresses
+
+
+# Example usage
+email_string = "alexa@amazon.com talk@talk.com john.jones@flatironschool.com"
+parser = EmailAddressParser(email_string)
+parsed_emails = parser.parse()
+print(parsed_emails)
--- lib/testing/email_address_parser_test.py
+++ lib/testing/email_address_parser_test.py
@@ -1,32 +1,44 @@
 from email_address_parser import EmailAddressParser
 
 class TestEmailAddressParser:
-    '''Class EmailAddressParser in email_address_parser.py'''
-
+    # Test if EmailAddressParser instantiates with a string
     def test_instantiates_with_string(self):
-        '''instantiates with a single argument, a string.'''
-        assert(EmailAddressParser("string string@string.com, mr. string, iamastring@icloud.com"))
+        email_parser = EmailAddressParser("string string@string.com, mr. string, iamastring@icloud.com")
+        assert isinstance(email_parser, EmailAddressParser)
 
+    # Test if EmailAddressParser has a parse method
     def test_has_parse_method(self):
-        '''contains a method called "parse".'''
-        assert(EmailAddressParser.parse)
+        email_parser = EmailAddressParser("dummy string")
+        assert hasattr(email_parser, "parse")
 
+    # Test if EmailAddressParser correctly parses emails with spaces
     def test_parses_emails_with_spaces(self):
-        '''finds emails with spaces in between.'''
-        parser = EmailAddressParser("talk@talk.com john.jones@flatironschool.com alexa@amazon.com")
-        assert(parser.parse() == ["alexa@amazon.com", "john.jones@flatironschool.com", "talk@talk.com"])
+        email_string = "talk@talk.com john.jones@flatironschool.com alexa@amazon.com"
+        parser = EmailAddressParser(email_string)
+        parsed_emails = parser.parse()
+        expected_emails = ["talk@talk.com", "john.jones@flatironschool.com", "alexa@amazon.com"]
+        assert parsed_emails == expected_emails
 
+    # Test if EmailAddressParser correctly parses emails with commas
     def test_parses_emails_with_commas(self):
-        '''finds emails with commas in between.'''
-        parser = EmailAddressParser("talk@talk.com,john.jones@flatironschool.com,alexa@amazon.com")
-        assert(parser.parse() == ["alexa@amazon.com", "john.jones@flatironschool.com", "talk@talk.com"])
+        email_string = "talk@talk.com,john.jones@flatironschool.com,alexa@amazon.com"
+        parser = EmailAddressParser(email_string)
+        parsed_emails = parser.parse()
+        expected_emails = ["talk@talk.com", "john.jones@flatironschool.com", "alexa@amazon.com"]
+        assert parsed_emails == expected_emails
 
+    # Test if EmailAddressParser correctly parses emails with commas and spaces
     def test_parses_emails_with_commas_and_spaces(self):
-        '''finds emails with commas and spaces in between.'''
-        parser = EmailAddressParser("talk@talk.com, john.jones@flatironschool.com, alexa@amazon.com")
-        assert(parser.parse() == ["alexa@amazon.com", "john.jones@flatironschool.com", "talk@talk.com"])
+        email_string = "talk@talk.com, john.jones@flatironschool.com, alexa@amazon.com"
+        parser = EmailAddressParser(email_string)
+        parsed_emails = parser.parse()
+        expected_emails = ["talk@talk.com", "john.jones@flatironschool.com", "alexa@amazon.com"]
+        assert parsed_emails == expected_emails
 
+    # Test if EmailAddressParser correctly parses emails, removes non-email strings
     def test_parses_emails_with_commas_and_spaces_and_non_emails(self):
-        '''finds emails with commas and spaces in between and removes non-email strings.'''
-        parser = EmailAddressParser("talk@talk.com, what john.jones@flatironschool.com, who alexa@amazon.com, where when why")
-        assert(parser.parse() == ["alexa@amazon.com", "john.jones@flatironschool.com", "talk@talk.com"])
+        email_string = "talk@talk.com, what john.jones@flatironschool.com, who alexa@amazon.com, where when why"
+        parser = EmailAddressParser(email_string)
+        parsed_emails = parser.parse()
+        expected_emails = ["talk@talk.com", "john.jones@flatironschool.com", "alexa@amazon.com"]
+        assert parsed_emails == expected_emails
